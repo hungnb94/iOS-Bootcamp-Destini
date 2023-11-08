e5e71d1c635d468de72331cad2bb5e8d0f60c39f
Hung Nguyen <hungnb94@gmail.com>
2023-11-08 11:44:47 +0700

    Handle click

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps"),
+    ]
+    
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if index + 1 < stories.count {
+            index += 1
+        } else {
+            index = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        let story = stories[index]
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
 
 }
 
