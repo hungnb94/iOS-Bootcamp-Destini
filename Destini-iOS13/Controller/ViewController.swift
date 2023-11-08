7de0cac36c77f131143c14e1bee84800b94b0f99
Hung Nguyen <hungnb94@gmail.com>
2023-11-08 14:27:58 +0700

    Convert to mvc

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,13 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    let stories = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps"),
-    ]
-    
-    var index = 0
+    var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +22,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        if index + 1 < stories.count {
-            index += 1
-        } else {
-            index = 0
-        }
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
         updateUI()
     }
     
     func updateUI() {
-        let story = stories[index]
+        let story = storyBrain.getStory()
         storyLabel.text = story.title
         choice1Button.setTitle(story.choice1, for: .normal)
         choice2Button.setTitle(story.choice2, for: .normal)
